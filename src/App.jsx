4ca56602c658c8054918bbf98bd6b5425926742d
indira-coopts <indira.endaya@co-opts.com>
2024-06-19 14:41:31 +0000

    practice d3-react github codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import './App.css';
+import { useEffect } from "react";
+import * as d3 from "d3";
 
 function App() {
+  useEffect(() => {
+    d3.selectAll(".text").style("color", "skyblue").text("Hello, D3.js!");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="texts">
+      <p className="text"></p>
+      <p className="text"></p>
+      <p className="text"></p>
+      <p className="text"></p>
     </div>
   );
 }
